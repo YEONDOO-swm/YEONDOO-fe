ef0766f17541c34682fa2723eead6df634eb30c8
coddingyun <igy2840@gmail.com>
2023-08-09 16:29:00 +0900

    BUG: 검색 결과 없음 표시

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -251,10 +251,7 @@ export const Home = () => {
               </div>
             ))
           ) :
-          
-          
-        ( 
-          (searchResults.answer && searchResults.answer !== "아니아니아니") && (searchType==='1'?(
+        (searchResults && ((searchResults.answer !== "아니아니아니") ? (searchType==='1'?(
           <Box sx={{height: '75vh', margin: '0 30px 0 10px', overflowY: 'scroll'}} className={scrollStyle.scrollBar}>
             {searchResults.papers.map((paper: any) => (
           <Card key={paper.paperId} sx={{ marginBottom: '15px', border: `1px solid ${color.mainGrey}`, padding: '15px 25px', pb: '18px', borderRadius: '15px', backgroundColor: color.mainGrey}}>
@@ -364,7 +361,11 @@ export const Home = () => {
     </Grid>
   </Grid> */}
 </div>))
- )}
+: (
+  <Box sx={{m:3}}>
+    <Typography> 검색 결과가 없습니다.</Typography>
+  </Box>
+)))}
     </div>
     )
 };
